6283d98649aff0d99fba97c5270fba56afc7e1c4
sshuvoo <safayanshuvo@gmail.com>
2024-01-20 01:29:34 +0600

    Highlight selected pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
-import cursor1 from './assets/cursor1.png';
-import cursor2 from './assets/cursor2.png';
-import cursor3 from './assets/cursor3.png';
-import cursor4 from './assets/cursor4.png';
-import cursor5 from './assets/cursor5.png';
+import pointers from './pointers';
 
 export default function App() {
    const [position, setPosition] = useState({ x: -1, y: -1 });
-   const [cursor, setCursor] = useState(cursor1);
+   const [cursor, setCursor] = useState(pointers[0]);
+
    return (
       <main
          onPointerMove={(e) => setPosition({ x: e.clientX, y: e.clientY })}
@@ -36,36 +33,19 @@ export default function App() {
                className="w-72 px-8 py-5 text-teal-500 bg-teal-500/20 hover:bg-teal-500/35 border-2 border-teal-500/60 mx-auto font-medium focus:outline-none"
             />
             <div className="space-x-4">
-               <button
-                  className="border p-1 border-pink-200/40"
-                  onClick={() => setCursor(cursor1)}
-               >
-                  <img src={cursor1} className="w-10 h-10" />
-               </button>
-               <button
-                  className="border p-1 border-pink-200/40"
-                  onClick={() => setCursor(cursor2)}
-               >
-                  <img src={cursor2} className="w-10 h-10" />
-               </button>
-               <button
-                  className="border p-1 border-pink-200/40"
-                  onClick={() => setCursor(cursor3)}
-               >
-                  <img src={cursor3} className="w-10 h-10" />
-               </button>
-               <button
-                  className="border p-1 border-pink-200/40"
-                  onClick={() => setCursor(cursor4)}
-               >
-                  <img src={cursor4} className="w-10 h-10" />
-               </button>
-               <button
-                  className="border p-1 border-pink-200/40"
-                  onClick={() => setCursor(cursor5)}
-               >
-                  <img src={cursor5} className="w-10 h-10" />
-               </button>
+               {pointers.map((pointer, i) => (
+                  <button
+                     key={i}
+                     className={`border p-1 ${
+                        cursor === pointer
+                           ? 'border-pink-200/80'
+                           : 'border-pink-200/40'
+                     }`}
+                     onClick={() => setCursor(pointer)}
+                  >
+                     <img src={pointer} className="w-10 h-10" />
+                  </button>
+               ))}
             </div>
          </div>
       </main>
---
+++ src/pointers.js
@@ -0,0 +1,9 @@
+import cursor1 from './assets/cursor1.png';
+import cursor2 from './assets/cursor2.png';
+import cursor3 from './assets/cursor3.png';
+import cursor4 from './assets/cursor4.png';
+import cursor5 from './assets/cursor5.png';
+
+const pointers = [cursor1, cursor2, cursor3, cursor4, cursor5];
+
+export default pointers;
